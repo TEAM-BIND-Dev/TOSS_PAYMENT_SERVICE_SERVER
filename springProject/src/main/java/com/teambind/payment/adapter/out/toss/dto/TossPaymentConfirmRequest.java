e2baaa42a9cf8fa345b9f1fada00fc1d36667748
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 15:22:44 +0900

    REFACTOR: Update TossPaymentConfirmRequest

--- springProject/src/main/java/com/teambind/payment/adapter/out/toss/dto/TossPaymentConfirmRequest.java
+++ springProject/src/main/java/com/teambind/payment/adapter/out/toss/dto/TossPaymentConfirmRequest.java
@@ -1,13 +1,13 @@
 package com.teambind.payment.adapter.out.toss.dto;
 
 public record TossPaymentConfirmRequest(
-        // 결제 키
-        String paymentKey,
-
-        // 주문 ID
-        String orderId,
-
-        // 결제 금액
-        Long amount
+		// 결제 키
+		String paymentKey,
+		
+		// 주문 ID
+		String orderId,
+		
+		// 결제 금액
+		Long amount
 ) {
 }
