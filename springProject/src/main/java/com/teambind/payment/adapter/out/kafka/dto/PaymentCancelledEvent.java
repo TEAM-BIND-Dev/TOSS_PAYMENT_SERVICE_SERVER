70260ef7303b7ab85a0c9ef6790cdd7173aecf8b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-14 15:22:20 +0900

    REFACTOR: Update PaymentCancelledEvent

--- springProject/src/main/java/com/teambind/payment/adapter/out/kafka/dto/PaymentCancelledEvent.java
+++ springProject/src/main/java/com/teambind/payment/adapter/out/kafka/dto/PaymentCancelledEvent.java
@@ -8,30 +8,30 @@ import com.teambind.payment.domain.Payment;
 import java.time.LocalDateTime;
 
 public record PaymentCancelledEvent(
-        // 결제 ID
-        String paymentId,
-
-        // 예약 ID
-        String reservationId,
-
-        // 주문 ID
-        String orderId,
-
-        // 결제 금액
-        Long amount,
-
-        // 취소 시각
-        @JsonSerialize(using = LocalDateTimeSerializer.class)
-        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-        LocalDateTime cancelledAt
+		// 결제 ID
+		String paymentId,
+		
+		// 예약 ID
+		String reservationId,
+		
+		// 주문 ID
+		String orderId,
+		
+		// 결제 금액
+		Long amount,
+		
+		// 취소 시각
+		@JsonSerialize(using = LocalDateTimeSerializer.class)
+		@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+		LocalDateTime cancelledAt
 ) {
-    public static PaymentCancelledEvent from(Payment payment) {
-        return new PaymentCancelledEvent(
-                payment.getPaymentId(),
-                payment.getReservationId(),
-                payment.getOrderId(),
-                payment.getAmount().getValue().longValue(),
-                payment.getCancelledAt()
-        );
-    }
+	public static PaymentCancelledEvent from(Payment payment) {
+		return new PaymentCancelledEvent(
+				payment.getPaymentId(),
+				payment.getReservationId(),
+				payment.getOrderId(),
+				payment.getAmount().getValue().longValue(),
+				payment.getCancelledAt()
+		);
+	}
 }
